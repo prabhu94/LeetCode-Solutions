8ac3dfa6cb842b5baf4152ea2c9ccc7c8749718a
Prabhat Turlapati <prabhatturlapati1@gmail.com>
2020-02-26 17:09:53 -0600

    Update implement-strstr.py

--- implement-strstr.py
+++ implement-strstr.py
@@ -3,11 +3,8 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if len(needle)==0:
             return 0
-        
-        for i in range(0, len(haystack), len(needle)):
-            if needle == haystack[i:i+len(needle)]:
-                print("yes", haystack[i:i+len(needle)])
-                return i
-            else:
-                return -1
-            
+        for i in range(0, len(haystack)):
+            if needle in haystack[i:i+len(needle)]:
+                return i    
+        return -1
+                    
